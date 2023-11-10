ae8f5bdbb525154b3f95108a4c2cdece15a9b3d6
vinod257 <vinod070420002@gmail.com>
2023-11-10 09:31:53 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+def v1(request):
+    return render(request,'v1.html')
+def v2(request):
+    return render(request,'v2.html')
+def v3(request):
+    return render(request,'v3.html')
+def v4(request):
+    return render(request,'v4.html')
+
---
+++ four_html_pages/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for four_html_pages project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('v1/',v1,name='v1'),
+    path('v2/',v2,name='v2'),
+    path('v3/',v3,name='v3'),
+    path('v4/',v4,name='v4'),
+]
